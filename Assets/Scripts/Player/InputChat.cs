9b97efb23979b8d99cec7cc1043223c31716a6e3
verzelea <arthurverzele@hotmail.com>
2022-03-28 10:56:18 +0200

    :wastebasket: remove unused using

--- Assets/Scripts/Player/InputChat.cs
+++ Assets/Scripts/Player/InputChat.cs
@@ -1,8 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using TMPro;
 using Mirror;
+using TMPro;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class InputChat : NetworkBehaviour
